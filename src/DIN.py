19179af725799b5689ad99fbcf3ba6f9c9777254
PaoloMasciullo <paolomasciullojz4@gmail.com>
2024-05-07 11:21:03 +0200

    compute loss on validation set and merge DIN with DIN_bpr

--- src/DIN.py
+++ src/DIN.py
@@ -14,11 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========================================================================
+import numpy as np
+import pandas as pd
 import torch
 from torch import nn
 from pandas.core.common import flatten
 from fuxictr.pytorch.models import BaseModel
 from fuxictr.pytorch.layers import FeatureEmbeddingDict, MLP_Block, DIN_Attention, Dice
+from utils.bpr import BPRLoss
+from fuxictr.pytorch.torch_utils import get_optimizer, get_loss
+import logging
+from tqdm import tqdm
+import sys
 
 
 class DIN(BaseModel):
@@ -43,11 +50,11 @@ class DIN(BaseModel):
                  net_regularizer=None,
                  **kwargs):
         super(DIN, self).__init__(feature_map,
-                                  model_id=model_id,
-                                  gpu=gpu,
-                                  embedding_regularizer=embedding_regularizer,
-                                  net_regularizer=net_regularizer,
-                                  **kwargs)
+                                      model_id=model_id,
+                                      gpu=gpu,
+                                      embedding_regularizer=embedding_regularizer,
+                                      net_regularizer=net_regularizer,
+                                      **kwargs)
         if not isinstance(din_target_field, list):
             din_target_field = [din_target_field]
         self.din_target_field = din_target_field
@@ -80,6 +87,7 @@ class DIN(BaseModel):
         self.compile(kwargs["optimizer"], kwargs["loss"], learning_rate)
         self.reset_parameters()
         self.model_to_device()
+        self.loss_name = kwargs["loss"]
 
     def forward(self, inputs):
         X = self.get_inputs(inputs)
@@ -99,6 +107,24 @@ class DIN(BaseModel):
         return_dict = {"y_pred": y_pred}
         return return_dict
 
+    def train_step(self, batch_data):
+        self.optimizer.zero_grad()
+        return_dict = self.forward(batch_data)
+        y_true = self.get_labels(batch_data)
+
+        if self.loss_name == 'bpr':
+            y_true = y_true.data.cpu().numpy().reshape(-1)
+            y_pred = return_dict["y_pred"].data.cpu().numpy().reshape(-1)
+            group_id = self.get_group_id(batch_data).data.cpu().numpy().reshape(-1)
+            return_dict_grouped, y_true_grouped = self.get_scores_grouped_by_impression(group_id, y_true, y_pred)
+            loss = self.compute_loss(return_dict_grouped, y_true_grouped)
+        else:
+            loss = self.compute_loss(return_dict, y_true)
+        loss.backward()
+        nn.utils.clip_grad_norm_(self.parameters(), self._max_gradient_norm)
+        self.optimizer.step()
+        return loss
+
     @staticmethod
     def get_embedding(field, feature_emb_dict):
         if type(field) == tuple:
@@ -106,3 +132,71 @@ class DIN(BaseModel):
             return torch.cat(emb_list, dim=-1)
         else:
             return feature_emb_dict[field]
+
+    def compile(self, optimizer, loss, lr):
+        self.optimizer = get_optimizer(optimizer, self.parameters(), lr)
+        if loss == 'bpr':
+            self.loss_fn = BPRLoss(max_positives=1)  # max_positives = npratio + 1
+        else:
+            self.loss_fn = get_loss(loss)
+
+    def get_scores_grouped_by_impression(self, group_id, y_true, y_pred):
+        score_df = pd.DataFrame({"group_index": group_id,
+                                 "y_true": y_true,
+                                 "y_pred": y_pred})
+
+        idxs = []
+        y_true_list = []
+        y_pred_list = []
+
+        for idx, df in score_df.groupby("group_index"):
+            idxs.append(idx)
+            y_true_list.append(df['y_true'].values)
+            y_pred_list.append(df['y_pred'].values)
+
+        return_dict = {'y_pred': torch.Tensor(np.array(y_pred_list))}
+
+        return return_dict, torch.Tensor(np.array(y_true_list))
+
+    def evaluate(self, data_generator, metrics=None):
+        val_loss = 0
+        self.eval()  # set to evaluation mode
+        with torch.no_grad():
+            y_pred_list = []
+            y_true_list = []
+            group_id_list = []
+            if self._verbose > 0:
+                data_generator = tqdm(data_generator, disable=False, file=sys.stdout)
+            for batch_data in data_generator:
+                return_dict = self.forward(batch_data)
+                y_pred = return_dict["y_pred"].data.cpu().numpy().reshape(-1)
+                y_true = self.get_labels(batch_data).data.cpu().numpy().reshape(-1)
+                group_id = self.get_group_id(batch_data).numpy().reshape(-1)
+
+                # compute loss on validation
+                if self.loss_name == 'bpr':
+                    return_dict_grouped, y_true_grouped = self.get_scores_grouped_by_impression(group_id,
+                                                                                                y_true,
+                                                                                                y_pred)
+                    loss = self.compute_loss(return_dict_grouped, y_true_grouped)
+                else:
+                    loss = self.compute_loss(return_dict, self.get_labels(batch_data))
+
+                val_loss += loss.item()
+
+                y_pred_list.extend(y_pred)
+                y_true_list.extend(y_true)
+                if self.feature_map.group_id is not None:
+                    group_id_list.extend(group_id)
+
+            y_pred = np.array(y_pred_list, np.float64)
+            y_true = np.array(y_true_list, np.float64)
+            group_id = np.array(group_id_list) if len(group_id) > 0 else None
+
+            if metrics is not None:
+                val_logs = self.evaluate_metrics(y_true, y_pred, metrics, group_id)
+            else:
+                val_logs = self.evaluate_metrics(y_true, y_pred, self.validation_metrics, group_id)
+            logging.info("Val loss: {:.6f}".format(val_loss / len(data_generator)))
+            logging.info('[Metrics] ' + ' - '.join('{}: {:.6f}'.format(k, v) for k, v in val_logs.items()))
+            return val_logs
