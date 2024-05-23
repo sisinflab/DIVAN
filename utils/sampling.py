adc8d3bd690aac83f1a035872d3ebb30100ac72a
Paolo Masciullo <93404566+PaoloMasciullo@users.noreply.github.com>
2024-05-23 20:24:53 +0200

    fix prepare_data_v1.py for demo

--- utils/sampling.py
+++ utils/sampling.py
@@ -1,28 +1,44 @@
 import polars as pl
 import os
+import zipfile
+import requests
+import io
 
-validation_demo = pl.scan_parquet("../data/Ebnerd_demo/validation/behaviors.parquet")
-validation_len = validation_demo.collect().shape[0]
-# Behaviours Large
-df_behaviours = pl.scan_parquet("../data/Ebnerd_large/validation/behaviors.parquet")
-# Prendiamo un sample da Behaviours Large
-sample = df_behaviours.collect().sample(validation_len, with_replacement=False)
-# Prendiamo user_id univoci
-unique_user_id = sample.select("user_id").unique().to_numpy().flatten()
-# Prendiamo lista di tutti gli articoli nelle inviews
-article_id_inview = sample.select("article_ids_inview").explode("article_ids_inview").rename(
-    {"article_ids_inview": "article_id"})
-# Leggiamo history prendendo solo utenti presenti nel sample di behaviours
-df_history = pl.scan_parquet("../data/Ebnerd_large/validation/history.parquet").filter(pl.col("user_id").is_in(unique_user_id))
-# Prendiamo gli articoli nella history
-article_id_history = df_history.collect().select("article_id_fixed").explode("article_id_fixed").rename(
-    {"article_id_fixed": "article_id"})
-# Concateniamo (articoli_history + article_inviews) e prendiamo quelli univoci
-article_id = pl.concat([article_id_inview, article_id_history]).unique().to_numpy().flatten()
-# Filtriamo articles
-df_articles = pl.scan_parquet("../data/Ebnerd_large/train/articles.parquet").filter(pl.col("article_id").is_in(article_id))
-# SALVATAGGIO FILE
-os.makedirs("../data/Ebnerd_demo/test2", exist_ok=True)
-df_articles.collect().write_parquet("../data/Ebnerd_demo/test2/articles.parquet")
-sample.write_parquet("../data/Ebnerd_demo/test2/behaviors.parquet")
-df_history.collect().write_parquet("../data/Ebnerd_demo/test2/history.parquet")
+def create_test_for_demo():
+    validation_demo = pl.scan_parquet("./validation/behaviors.parquet")
+    validation_len = validation_demo.collect().shape[0]
+    # Behaviours Large
+    df_behaviours = pl.scan_parquet("../Ebnerd_large/validation/behaviors.parquet")
+    # Prendiamo un sample da Behaviours Large
+    sample = df_behaviours.collect().sample(validation_len, with_replacement=False)
+    # Prendiamo user_id univoci
+    unique_user_id = sample.select("user_id").unique().to_numpy().flatten()
+    # Prendiamo lista di tutti gli articoli nelle inviews
+    article_id_inview = sample.select("article_ids_inview").explode("article_ids_inview").rename(
+        {"article_ids_inview": "article_id"})
+    # Leggiamo history prendendo solo utenti presenti nel sample di behaviours
+    df_history = pl.scan_parquet("../Ebnerd_large/validation/history.parquet").filter(
+        pl.col("user_id").is_in(unique_user_id))
+    # Prendiamo gli articoli nella history
+    article_id_history = df_history.collect().select("article_id_fixed").explode("article_id_fixed").rename(
+        {"article_id_fixed": "article_id"})
+    # Concateniamo (articoli_history + article_inviews) e prendiamo quelli univoci
+    article_id = pl.concat([article_id_inview, article_id_history]).unique().to_numpy().flatten()
+    # Filtriamo articles
+    df_articles = pl.scan_parquet("../Ebnerd_large/train/articles.parquet").filter(
+        pl.col("article_id").is_in(article_id))
+    # SALVATAGGIO FILE
+    os.makedirs("./test2", exist_ok=True)
+    df_articles.collect().write_parquet("./test2/articles.parquet")
+    sample.write_parquet("./test2/behaviors.parquet")
+    df_history.collect().write_parquet("./test2/history.parquet")
+
+def create_test_for_large():
+    dataset_url = f"https://ebnerd-dataset.s3.eu-west-1.amazonaws.com/ebnerd_small.zip"
+    response_dataset = requests.get(dataset_url)
+    with zipfile.ZipFile(io.BytesIO(response_dataset.content)) as zip_ref:
+        zip_ref.extract("articles.parquet", path="./train/")
+        zip_ref.extract("validation/history.parquet")
+        zip_ref.extract("validation/behaviors.parquet")
+    os.rename("./validation/history.parquet", "./test2/history.parquet")
+    os.rename("./validation/behaviors.parquet", "./test2/behaviors.parquet")
