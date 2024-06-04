d2f08d1cd00eae47aa8d26463c6de1e537afad7b
AntoDeca1 <antoniodecandia01@gmail.com>
2024-06-04 09:57:10 +0200

    submit_popular.py fix

--- submit_popular.py
+++ submit_popular.py
@@ -22,7 +22,7 @@ ans = ans.select(['impression_id', 'popularity_score'])
 logging.info("Predicting scores...")
 ans = ans.rename({'popularity_score': 'score'}).collect().to_pandas()
 logging.info("Ranking samples...")
-ans = ans.groupby(['impression_id'], sort=False).apply(grank).reset_index(drop=True)
+ans = ans.groupby(['impression_id', 'user_id'], sort=False).apply(grank).reset_index(drop=True)
 logging.info("Writing results...")
 os.makedirs("submit", exist_ok=True)
 with open('submit/predictions.txt', "w") as fout:
