5634808b1a39db33a486e195a0aa17727978f3b2
Paolo Masciullo <93404566+PaoloMasciullo@users.noreply.github.com>
2024-06-26 15:11:44 +0200

    memory optimization of preprocess_dataset_split.py

--- preprocess_dataset_split.py
+++ preprocess_dataset_split.py
@@ -15,50 +15,95 @@
 # =========================================================================
 
 
-import sys
 import fuxictr_version
 import logging
 
-from fuxictr import datasets
-from datetime import datetime
-from fuxictr.utils import load_config, set_logger, print_to_json, print_to_list
-from fuxictr.features import FeatureMap
-from utils.dataset_utils import RankDataLoader
+from fuxictr.utils import load_config, set_logger, print_to_json
 from fuxictr.pytorch.torch_utils import seed_everything
-from fuxictr.preprocess import FeatureProcessor, transform
-import src
+from fuxictr.preprocess import FeatureProcessor
 import gc
 import argparse
 import os
-from pathlib import Path
 import warnings
+import multiprocessing as mp
+import numpy as np
+import polars as pl
 
 warnings.filterwarnings("ignore")
+
+
+def save_npz(darray_dict, data_path):
+    logging.info("Saving data to npz: " + data_path)
+    os.makedirs(os.path.dirname(data_path), exist_ok=True)
+    np.savez(data_path, **darray_dict)
+
+
+def transform_block(feature_encoder, df_block, filename):
+    darray_dict = feature_encoder.transform(df_block)
+    save_npz(darray_dict, os.path.join(feature_encoder.data_dir, filename))
+
+
+def transform(feature_encoder, ddf, filename, block_size=0):
+    logging.info("Transform feature columns...")
+    ddf = ddf.to_pandas()
+    if block_size > 0:
+        pool = mp.Pool(mp.cpu_count() // 2)
+        block_id = 0
+        for idx in range(0, len(ddf), block_size):
+            df_block = ddf.iloc[idx:(idx + block_size)]
+            pool.apply_async(
+                transform_block,
+                args=(feature_encoder,
+                      df_block,
+                      '{}/part_{:05d}.npz'.format(filename, block_id))
+            )
+            block_id += 1
+        del df_block
+        gc.collect()
+
+        pool.close()
+        pool.join()
+    else:
+        transform_block(feature_encoder, ddf, filename)
 
 
 def transform_split(feature_encoder, train_data=None, valid_data=None, test_data=None, data_block_size=0, **kwargs):
     # fit and transform train_ddf
     if train_data:
         train_ddf = feature_encoder.read_csv(train_data, **kwargs)
-        train_ddf = feature_encoder.preprocess(train_ddf)
-        # feature_encoder.fit(train_ddf, **kwargs)
+        train_ddf_list = []
+        for df in train_ddf.collect().iter_slices(data_block_size):
+            train_ddf_list.append(feature_encoder.preprocess(df))
+            del df
+            gc.collect()
+        train_ddf = pl.concat(train_ddf_list)
         transform(feature_encoder, train_ddf, 'train', block_size=data_block_size)
-        del train_ddf
+        del train_ddf, train_ddf_list
         gc.collect()
 
     if valid_data:
         valid_ddf = feature_encoder.read_csv(valid_data, **kwargs)
-        valid_ddf = feature_encoder.preprocess(valid_ddf)
+        valid_ddf_list = []
+        for df in valid_ddf.collect().iter_slices(data_block_size):
+            valid_ddf_list.append(feature_encoder.preprocess(df))
+            del df
+            gc.collect()
+        valid_ddf = pl.concat(valid_ddf_list)
         transform(feature_encoder, valid_ddf, 'valid', block_size=data_block_size)
-        del valid_ddf
+        del valid_ddf, valid_ddf_list
         gc.collect()
 
     # Transfrom test_ddf
     if test_data:
         test_ddf = feature_encoder.read_csv(test_data, **kwargs)
-        test_ddf = feature_encoder.preprocess(test_ddf)
+        test_ddf_list = []
+        for df in test_ddf.collect().iter_slices(data_block_size):
+            test_ddf_list.append(feature_encoder.preprocess(df))
+            del df
+            gc.collect()
+        test_ddf = pl.concat(test_ddf_list)
         transform(feature_encoder, test_ddf, 'test', block_size=data_block_size)
-        del test_ddf
+        del test_ddf, test_ddf_list
         gc.collect()
     logging.info("Transform csv data to npz done.")
 
